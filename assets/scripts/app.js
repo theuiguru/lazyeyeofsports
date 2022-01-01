b8a20b347dcc257e75f46ba1af75661c66faf29c
Chris Thomas <webmaster102@gmail.com>
2021-12-31 23:33:23 -0500

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,9 @@
-var bgimg = ['nba', 'nfl', 'mlb'],
+let bgimg = ['nba', 'nfl', 'mlb'],
     b = Math.floor(Math.random() * bgimg.length),
     body = document.querySelector("body");
 body.style.background = 'url(./assets/images/' + bgimg[b] + '_bg.jpg) no-repeat fixed center';
 body.style.backgroundSize = 'cover';
-var today = new Date(); 
-var year = today.getFullYear(); 
-var el = document.querySelector(".year"); 
-el.innerText = year;
+document.querySelector(".year").innerText = new Date().getFullYear();
 async function fetchWPdata() {
 	await fetch('https://public-api.wordpress.com/rest/v1/sites/thelazyeyeofsports.wordpress.com/posts')
     .then(res => res.json())
